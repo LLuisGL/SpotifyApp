05ad5dba361d34dfbf048aafa44be6b8a144c840
Luis Goncalves <154645283+LLuisGL@users.noreply.github.com>
2025-02-04 22:09:53 -0500

    change

--- src/main/java/ec/edu/uees/spotifyapp/App.java
+++ src/main/java/ec/edu/uees/spotifyapp/App.java
@@ -17,7 +17,7 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("loading"), 420, 720);
+        scene = new Scene(loadFXML("loading"), 1000, 720);
         stage.setScene(scene);
         stage.show();
     }
