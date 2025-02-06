1444bc8f0d67671e42aa65b92b2d8f0c8ee6458e
carloselcrackt1 <carlosalejandroauquimolina@gmail.com>
2025-02-05 20:19:25 -0500

    updateDoubleLinkedCircleList
     Se crearon las listas ciclicas dobles y se le agregaron sus metodos correspondiente.

--- src/main/java/ListaCircular/DoubleLinkedCircleList.java
+++ src/main/java/ListaCircular/DoubleLinkedCircleList.java
@@ -8,6 +8,7 @@ package ListaCircular;
  * @author Carlos Auqui
  */
 public class DoubleLinkedCircleList<E> implements Lista<E>{
+    private Node<E> first;
     private Node<E> last;
     private int current;
    
@@ -15,7 +16,6 @@ public class DoubleLinkedCircleList<E> implements Lista<E>{
         E data;
         Node<E> next;
         Node<E> prev;
-        private int current;
         
         public Node(E e){
             this.data = e;
@@ -26,65 +26,176 @@ public class DoubleLinkedCircleList<E> implements Lista<E>{
     
     @Override
     public boolean addLast(E e) {
+        Node<E> p = new Node<>(e);
         if(e == null){ 
             return false;
         }
-        Node<E> p = new Node<>(e);
+
         if(this.isEmpty()){
-        last = p;
-        last.prev = p;
-        last.next = p;
+            first = last = p;
         }
         
-        p.next = last.next;
-        last.next = p; 
-        return true;
+        p.next = last.next; // el nuevo nodo apunta al inicio 
+        first.prev = p; // el previo del nodo inicial va a apuntar al nuevo nodo
+        last.next = p; // el que en ese momento es el ultimo nodo va a apuntar ahora al nuevo nodo
+        p.prev = last; // el previo del nuevo nodo apuntara al que es el ultimo en ese momento
+        last = p; // ahora el ultimo sera el nodo ingresado
+        current++;
+        return true;       
     }
 
     @Override
     public boolean addFirst(E e) {
-        throw new UnsupportedOperationException("Not supported yet."); 
+        Node<E> p = new Node<>(e);
+        if(e == null){
+            return false;
+        }
+        if(this.isEmpty()){
+            first = last = p;
+        }
+        p.prev = first.prev; // el nuevo nodo apuntara a la misma direccion que el previo del primero
+        first.prev = p; // ahora el previo apuntara a el nuevo nodo
+        p.next = first;// el next del nuevo nodo apuntara a p
+        last.next = p; // el next del ultimo nodo apuntara a p
+        first = p; // ahora el primero nodo sera p,,
+        current++;
+        return true;
     }
 
     @Override
     public E removeFirst() {
-        throw new UnsupportedOperationException("Not supported yet.");   
+        if (this.isEmpty()){
+            return null;
+        }
+        if (current == 1){
+            first= last = null;
+        }
+        E tmp = first.data; // aseguramos el dato del primer elemento para devolver
+        last.next = first.next; // el next del ultimo nodo va a apuntar al next del primer nodo
+        first.next.prev = first.prev; // el prev del next del primer nodo apuntara donde apunta el prev del primero nodo
+        first.prev = null; //desconectamos el prev del  primer arreglo
+        Node<E> tmp1 = first; // creamos un nodo que se situara en la ubicacion
+        first = first.next; // ahora se ajusta el nodo siguiente como first
+        tmp1.next = null; // desconectamos el nodo que era first del arreglo
+        current--;
+        return tmp; 
     }
 
     @Override
     public E removeLast() {
-        throw new UnsupportedOperationException("Not supported yet.");  
+      if (this.isEmpty()){
+            return null;
+        }
+        if (current == 1){
+            first= last = null;
+        }
+        E tmp = last.data; // resguardamos los datos del ultimo nodo
+        last.prev.next = last.next; // usamos el nodo previo para que apunte al nodo inicial
+        last.next =null; // ahora el next del ultimo para desconectar
+        first.prev = last.prev; // ahora el previo del primero apuntara al previo del ultimo
+        Node<E> tmp1 = last; // se crea un nodo tmp que se situara en el ultimo nodo
+        last = last.prev; // ahora el nodo previo sera el ultimo
+        tmp1.prev =null; // desde el nodo tmp se desconectara la ultima referencia 
+        current--;
+        return tmp;
     }
 
     @Override
     public E get(int index) {
-        throw new UnsupportedOperationException("Not supported yet.");  
+        if(this.isEmpty()){
+            return null;
+        }
+        if(index <0 || index > current){
+            throw new IndexOutOfBoundsException("Indice no permitido");
+        }
+        Node<E> getter = SetUbicacion(index);
+        E tmp = getter.data; //obtenemos el dato solicitado
+        return tmp; 
     }
 
     @Override
     public boolean isEmpty() {
-        Boolean validacion = (last == null);
+        Boolean validacion = (first == null && last == null);
         return validacion;  
     }
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException("Not supported yet.");  
+        return current; 
     }
 
     @Override
-    public E remove(int e) {
-        throw new UnsupportedOperationException("Not supported yet.");  
+    public E remove(int index) {
+        if(this.isEmpty()){ 
+            return null;
+        }
+        if(index <0 || index > current){
+            throw new IndexOutOfBoundsException("Indice no permitido");
+        }
+        if(index == 0){
+            removeFirst();
+        }
+        if(index == current-1){
+            removeLast();
+        }
+        Node<E> p = SetUbicacion(index); // ubicamos el nodo en el indice correspondiente
+        Node<E> tmp1 = p; // ubicamos otro nodo temporal donde apunta p
+        E tmp = p.data; // respaldamos la informacion de p
+        p.next.prev = p.prev; // el nodo siguiente a p apunta al previo de p
+        p.prev.next = p.next; // el nodo previo a p apunta al siguietne de p
+        p.prev = null; // p ponde en nulo la referencia previa
+        p = p.next; // ahora p se convierte en el nodo siguiente
+        tmp1.next = null; // el nodo temporal sirve para poner en nulo la ultima referencia de p
+        current--;
+        return tmp;
     }
 
     @Override
     public boolean add(int index, E e) {
-        throw new UnsupportedOperationException("Not supported yet.");  
+        if(this.isEmpty()){ 
+            return false;
+        }
+        if(index <0 || index > current){
+            throw new IndexOutOfBoundsException("Indice no permitido");
+        }
+        if(index == 0){
+            addFirst(e);
+        }
+        if(index == current-1){
+            addLast(e);
+        }
+        Node<E> p = SetUbicacion(index); // p se situa en el nodo que vamos a manipular para agg el nodeU
+        Node<E> NodeU = new Node<>(e); // Nodo que vamos a agg en el indice indicado
+        p.prev.next = NodeU; // hacemos que el nodo anterior a p apunte al nodeU que vamos a agg
+        NodeU.next = p.next.prev; // hacemos que el nodeU apunte a p
+        NodeU.prev = p.prev; // el prev de NodeU va a apuntar al prev de p
+        p.prev = NodeU; // hacemos que el prev de p apunte al nuevo node agg        
+        p = NodeU; // hacemos que NodeU se convierta en p, debido a los indices
+        current++; 
+        return true;    
     }
 
     @Override
     public boolean contains(E e) {
-        throw new UnsupportedOperationException("Not supported yet.");  
+        if(this.isEmpty()){
+            return false;
+        } 
+        Node<E> comp = first;
+        for (int i = 0; i<current;i++){ //metodo que te va comparando cada data del nodo con equals
+           if(comp.data.equals(e)){
+               return true;
+           }
+           comp = comp.next;
+        }
+        return false;
+    }
+    
+    private Node<E> SetUbicacion(int Index){
+        Node<E> NodeUbi = first;
+        for(int i= 0 ; i<Index;i++){
+            NodeUbi = NodeUbi.next; // Este metodo nos permite situarnos un nodo antes del que nos pide el usuario, y el next no lleva al solicitado.
+        }
+        return NodeUbi;
     }
     
 }
