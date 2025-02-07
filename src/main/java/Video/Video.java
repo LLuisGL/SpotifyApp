bb535f7606b9959b0a38a9325104558f209a3861
Luis Goncalves <154645283+LLuisGL@users.noreply.github.com>
2025-02-07 00:36:36 -0500

    new attributes

--- src/main/java/Video/Video.java
+++ src/main/java/Video/Video.java
@@ -1,17 +1,24 @@
 package Video;
 
+import java.time.Duration;
+
 /**
  *
  * @author Carlos Auqui
  */
 public class Video {
-    private String nombre_video;
+    private String nombreVideo;
+    private String artistaVideo;
+    private String colorVideo;
+    
     private String ruta;
-    private double duracion;
+    private Duration duracion;
 
-    public Video(String nombre_video, String ruta) {
-        this.nombre_video = nombre_video;
+    public Video(String nombreVideo, String artistaVideo, String colorVideo, String ruta, Duration duracion) {
+        this.nombreVideo = nombreVideo;
+        this.artistaVideo = artistaVideo;
         this.ruta = ruta;
+        this.duracion = duracion;
     }
 
     public String getRuta() {
@@ -23,19 +30,43 @@ public class Video {
     }
 
     public String getNombre_video() {
-        return nombre_video;
+        return nombreVideo;
     }
 
     public void setNombre_video(String nombre_video) {
-        this.nombre_video = nombre_video.toUpperCase();
+        this.nombreVideo = nombreVideo.toUpperCase();
     }
 
-    public double getDuracion() {
+    public Duration getDuracion() {
         return duracion;
     }
 
-    public void setDuracion(double duracion) {
+    public void setDuracion(Duration duracion) {
         this.duracion = duracion;
+    }
+
+    public String getNombreVideo() {
+        return nombreVideo;
+    }
+
+    public void setNombreVideo(String nombreVideo) {
+        this.nombreVideo = nombreVideo;
+    }
+
+    public String getArtistaVideo() {
+        return artistaVideo;
+    }
+
+    public void setArtistaVideo(String artistaVideo) {
+        this.artistaVideo = artistaVideo;
+    }
+
+    public String getColorVideo() {
+        return colorVideo;
+    }
+
+    public void setColorVideo(String colorVideo) {
+        this.colorVideo = colorVideo;
     }
    
     
